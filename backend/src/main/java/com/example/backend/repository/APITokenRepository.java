6b4401a549890d07394742af65c54197df123c92
kpryu6 <kyongpyo0626@gmail.com>
2024-12-18 15:02:12 +0900

    fix: rename TokenRepository to APITokenRepository

--- backend/src/main/java/com/example/backend/repository/APITokenRepository.java
+++ backend/src/main/java/com/example/backend/repository/APITokenRepository.java
@@ -6,12 +6,12 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface TokenRepository extends JpaRepository<APIToken, Integer> {
+public interface APITokenRepository extends JpaRepository<APIToken, Integer> {
 
     // 특정 토큰 값으로 조회
     Optional<APIToken> findByTokenValue(String tokenValue);
 
     // 만료된 토큰 삭제
-    @Query("DELETE FROM TokenStore t WHERE t.expirationTime < CURRENT_TIMESTAMP")
+    @Query("DELETE FROM Token t WHERE t.expirationTime < CURRENT_TIMESTAMP")
     void deleteExpiredTokens();
 }

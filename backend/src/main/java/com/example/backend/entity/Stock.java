fae654c962e66e9cf5d5fa31e8076e3f641ee516
kpryu6 <kyongpyo0626@gmail.com>
2024-12-18 14:49:56 +0900

    add: add stock_id in Stock Entity & add findById in StockRepository

--- backend/src/main/java/com/example/backend/entity/Stock.java
+++ backend/src/main/java/com/example/backend/entity/Stock.java
@@ -8,18 +8,22 @@ public class Stock {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private int id;
 
     @Column(name = "stock_name")
     private String stockName;
 
+    @Column(name = "stock_id")
+    private String stockId;
+
     public Stock() {}
 
-    public Stock(String stockName) {
+    public Stock(String stockName, String stockId) {
         this.stockName = stockName;
+        this.stockId = stockId;
     }
 
-    public String getId() {
+    public Integer getId() {
         return id;
     }
     public String getStockName() {
--- backend/src/main/java/com/example/backend/repository/StockRepository.java
+++ backend/src/main/java/com/example/backend/repository/StockRepository.java
@@ -1,7 +1,12 @@
 package com.example.backend.repository;
 
 import com.example.backend.entity.Stock;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StockRepository extends JpaRepository<Stock, String> {
+public interface StockRepository extends JpaRepository<Stock, Integer> {
+
+    Optional<Stock> findByStockId(String stockId);
 }

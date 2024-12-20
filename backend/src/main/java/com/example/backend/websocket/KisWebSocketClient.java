94f070ef06260c5ab0511532a7b2135a523bb753
naboyeong <nabo2000@naver.com>
2024-12-20 11:47:55 +0900

    add: 필요 데이터 필터링

--- backend/src/main/java/com/example/backend/websocket/KisWebSocketClient.java
+++ backend/src/main/java/com/example/backend/websocket/KisWebSocketClient.java
@@ -37,7 +37,7 @@ public class KisWebSocketClient {
             @Override
             public void onOpen(WebSocket webSocket, Response response) {
                 log.info("웹소켓 연결 성공");
-                String[] subscriptionlist = {"K1", "Q1"}; //구독할 리스트
+                String[] subscriptionlist = {"005930", "000660"}; //구독할 리스트(삼성전자, sk하이닉스)
                 subscribeStocks(subscriptionlist);
             }
             
@@ -58,62 +58,22 @@ public class KisWebSocketClient {
                         String[] responsedata = text_response_messages.split("\\^");
                         
                         for (int i = 0; i < text_data_number; i++) {
-                            String MKSC_SHRN_ISCD = responsedata[i * 46];
-                            String STCK_CNTG_HOUR = responsedata[i * 46 + 1];
-                            String STCK_PRPR = responsedata[i * 46 + 2];
-                            String PRDY_VRSS_SIGN = responsedata[i * 46 + 3];
-                            String PRDY_VRSS = responsedata[i * 46 + 4];
-                            String PRDY_CTRT = responsedata[i * 46 + 5];
-                            String WGHN_AVRG_STCK_PRC = responsedata[i * 46 + 6];
-                            String STCK_OPRC = responsedata[i * 46 + 7];
-                            String STCK_HGPR = responsedata[i * 46 + 8];
-                            String STCK_LWPR = responsedata[i * 46 + 9];
-                            String ASKP1 = responsedata[i * 46 + 10];
-                            String BIDP1 = responsedata[i * 46 + 11];
-                            String CNTG_VOL = responsedata[i * 46 + 12];
-                            String ACML_VOL = responsedata[i * 46 + 13];
-                            String ACML_TR_PBMN = responsedata[i * 46 + 14];
-                            String SELN_CNTG_CSNU = responsedata[i * 46 + 15];
-                            String SHNU_CNTG_CSNU = responsedata[i * 46 + 16];
-                            String NTBY_CNTG_CSNU = responsedata[i * 46 + 17];
-                            String CTTR = responsedata[i * 46 + 18];
-                            String SELN_CNTG_SMTN = responsedata[i * 46 + 19];
-                            String SHNU_CNTG_SMTN = responsedata[i * 46 + 20];
-                            String CCLD_DVSN = responsedata[i * 46 + 21];
-                            String SHNU_RATE = responsedata[i * 46 + 22];
-                            String PRDY_VOL_VRSS_ACML_VOL_RATE = responsedata[i * 46 + 23];
-                            String OPRC_HOUR = responsedata[i * 46 + 24];
-                            String OPRC_VRSS_PRPR_SIGN = responsedata[i * 46 + 25];
-                            String OPRC_VRSS_PRPR = responsedata[i * 46 + 26];
-                            String HGPR_HOUR = responsedata[i * 46 + 27];
-                            String HGPR_VRSS_PRPR_SIGN = responsedata[i * 46 + 28];
-                            String HGPR_VRSS_PRPR = responsedata[i * 46 + 29];
-                            String LWPR_HOUR = responsedata[i * 46 + 30];
-                            String LWPR_VRSS_PRPR_SIGN = responsedata[i * 46 + 31];
-                            String LWPR_VRSS_PRPR = responsedata[i * 46 + 32];
-                            String BSOP_DATE = responsedata[i * 46 + 33];
-                            String NEW_MKOP_CLS_CODE = responsedata[i * 46 + 34];
-                            String TRHT_YN = responsedata[i * 46 + 35];
-                            String ASKP_RSQN1 = responsedata[i * 46 + 36];
-                            String BIDP_RSQN1 = responsedata[i * 46 + 37];
-                            String TOTAL_ASKP_RSQN = responsedata[i * 46 + 38];
-                            String TOTAL_BIDP_RSQN = responsedata[i * 46 + 39];
-                            String VOL_TNRT = responsedata[i * 46 + 40];
-                            String PRDY_SMNS_HOUR_ACML_VOL = responsedata[i * 46 + 41];
-                            String PRDY_SMNS_HOUR_ACML_VOL_RATE = responsedata[i * 46 + 42];
-                            String HOUR_CLS_CODE = responsedata[i * 46 + 43];
-                            String MRKT_TRTM_CLS_CODE = responsedata[i * 46 + 44];
-                            String VI_STND_PRC = responsedata[i * 46 + 45];
-
-                            // 여기에서 각 데이터를 처리하거나 저장하는 로직을 추가할 수 있습니다.
-                            // 예: 데이터베이스에 저장하거나 화면에 표시하는 등의 작업
-                            log.info("현재가(STCK_PRPR): {}, 체결시간(STCK_CNTG_HOUR): {}", 
-                                STCK_PRPR, STCK_CNTG_HOUR);
+                            String MKSC_SHRN_ISCD = responsedata[i * 46]; //유가증권 단축 종목코드
+                            String STCK_CNTG_HOUR = responsedata[i * 46 + 1]; //주식 체결 시간
+                            String STCK_PRPR = responsedata[i * 46 + 2]; //주식 현재가
+                            String PRDY_VRSS_SIGN = responsedata[i * 46 + 3]; //전일 대비 부호
+                            String PRDY_CTRT = responsedata[i * 46 + 5]; // 전일 대비율
+                            String CNTG_VOL = responsedata[i * 46 + 12]; //체결 거래량
+        
+                            //테스트용
+                            log.info("유가증권 단축 종목코드(MKSC_SHRN_ISCD): {}, 주식 현재가(STCK_PRPR): {}, 주식 체결 시간(STCK_CNTG_HOUR): {}, 전일 대비 부호 (PRDY_VRSS_SIGN) : {}, 전일 대비율(PRDY_CTRT): {}, 체결 거래량(CNTG_VOL) : {}", 
+                                MKSC_SHRN_ISCD, STCK_PRPR, STCK_CNTG_HOUR, PRDY_VRSS_SIGN, PRDY_CTRT, CNTG_VOL);
                             
-                            if (MKSC_SHRN_ISCD.equals("K1")) {
-                                log.info("코스피 지수: {}, 시간: {}", STCK_PRPR, STCK_CNTG_HOUR);
-                            } else if (MKSC_SHRN_ISCD.equals("Q1")) {
-                                log.info("코스닥 지수: {}, 시간: {}", STCK_PRPR, STCK_CNTG_HOUR);
+                            //테스트용
+                            if (MKSC_SHRN_ISCD.equals("005930")) {
+                                log.info("삼성전자 현재가: {}, 시간: {}", STCK_PRPR, STCK_CNTG_HOUR);
+                            } else if (MKSC_SHRN_ISCD.equals("000660")) {
+                                log.info("sk하이닉스 현재가: {}, 시간: {}", STCK_PRPR, STCK_CNTG_HOUR);
                             }
                             
 

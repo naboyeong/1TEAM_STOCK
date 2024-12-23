67471955f678292af01af858b6aa0c5e677aa0ca
kpryu6 <kyongpyo0626@gmail.com>
2024-12-23 13:23:04 +0900

    chore: delete entity & repo about redis

--- backend/src/main/java/com/example/backend/config/RedisConfig.java
+++ backend/src/main/java/com/example/backend/config/RedisConfig.java
@@ -10,6 +10,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    // Spring Boot의 기본 설정 -> Key와 Value가 Object로 직렬화됨 -> 문제 발생 가능
+    // 모든 데이터를 String으로 저장, 관리하기 위해서 RedisTemplate의 Key와 Value Serializer를 StringRedisSerializer로 설정
     @Bean
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, String> template = new RedisTemplate<>();
--- backend/src/main/java/com/example/backend/repository/redis/RealTimeRedisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.repository.redis;
-
-import com.example.backend.entity.RealTimeRedis;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RealTimeRedisRepository extends CrudRepository<RealTimeRedis, String> {
-    // Redis Repository 코드
-}

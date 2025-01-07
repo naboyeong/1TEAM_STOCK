7b4534c79fc4627234cfc690ff5fcd6b4d42d2f9
woduf1020 <87353985+woduf1020@users.noreply.github.com>
2025-01-07 10:33:51 +0900

    Revert "feature : frontend-ecs"

--- backend/README.md
+++
@@ -1,7 +0,0 @@
-# 등락률 순위 수정 중이에요
-
-## 키 수동 설정필요
-- src/main/src/main/resources/application.properties
-- 위의 경로에서 키 수동으로 넣고 실행해주세요
-
-## 아직 우리가 원하는 급상승, 하락 정보가 안나옴
--- backend/src/main/java/com/example/backend/repository/DailyStockPriceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend.repository;
-
-import com.example.backend.entity.DailyStockPrice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DailyStockPriceRepository extends JpaRepository<DailyStockPrice, Integer> {
-}

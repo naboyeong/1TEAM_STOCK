1fe623c8b077940efbb43f43744666d54972028e
woduf1020 <87353985+woduf1020@users.noreply.github.com>
2025-01-13 11:14:08 +0900

    Backend코드가 왜다른지 모르겠는데 develop과 동일하게 수정

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -4,11 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableKafka // Kafka 활성화
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.example.backend.repository.jpa")
-@EnableRedisRepositories(basePackages = "com.example.backend.repository.redis")
+@EnableScheduling
+@EnableJpaRepositories(basePackages = "com.example.backend.repository")
 public class BackendApplication {
 
     public static void main(String[] args) {

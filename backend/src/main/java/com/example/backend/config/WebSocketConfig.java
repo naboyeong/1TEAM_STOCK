605fcc7b84dce47a5c1d099f7471a1115ae421c7
kpryu6 <kyongpyo0626@gmail.com>
2025-01-22 09:44:33 +0900

    fix origin

--- backend/src/main/java/com/example/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/example/backend/config/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(stockWebSocketHandler(), "/ws/stock")
-                .setAllowedOrigins("*"); // 특정 도메인만 허용
+                .setAllowedOrigins("https://1thebest.site"); // 특정 도메인만 허용
     }
 
     @Bean
